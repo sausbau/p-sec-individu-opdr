d752ddf7caf83dab938e8c5385d4c33d3eeca4f5
sausbau <sergioadssousa02@gmail.com>
2022-11-17 15:34:08 +0100

    laatste puntjes op de i

--- individu project/app.py
+++ individu project/app.py
@@ -15,35 +15,34 @@ recaptcha = ReCaptcha(app)
 @app.route("/", methods=['GET', 'POST'])
 def login():
     message = ''
-
-    with open('database.json', 'r') as f:
+    with open('database.json', 'r') as f: #here it wil open the json file with the database entries
         db = json.load(f)
 
     if request.method == 'POST':
         ip = request.environ['REMOTE_ADDR']
         with open ("ip.log", "a") as file:
-            file.write(f"{datetime.now()} - {ip}\n") #write the ip and time to ip.log file to monitor the amount of post requests
+            file.write(f"{datetime.now()} - {ip}\n") #write the ip and time to ip.log file to monitor the amount of post requests (login attempts)
 
-        if recaptcha.verify():
+        if recaptcha.verify(): #here it wil check if the captcha is verified or not
                 username = request.form["usname"]
                 password = request.form["psw"]
 
-                if username == db["username"]:
-                    if password == db["password"] and db['F_attemps'] > 0:
-                        db["F_attemps"] = 3
-                        with open('database.json', 'w') as f:
+                if username == db["username"]: #if the username input is in the json file it executes the code here underneath
+                    if password == db["password"] and db['F_attemps'] > 0: #if the password is correct and the attempts is higher than 0 
+                        db["F_attemps"] = 3 #here it resets the number back to 3 if the username and password are correct of the user
+                        with open('database.json', 'w') as f: #here it writes the 3 to the json file
                             json.dump(db, f)
                         return redirect(url_for('succes'))
 
-                    elif  db['F_attemps'] <= 0:
+                    elif  db['F_attemps'] <= 0: #here it checks if the amount of attempts is lower than 0 or is 0 if thats the case it will print the message underneath
                         Wmessage = 'Your account has been locked please contact us for more information'
                         return render_template('login.html', message=Wmessage)
                     else:
-                        db["F_attemps"] -= 1
+                        db["F_attemps"] -= 1 #if the password is incorrect of the user it wil reduce the ammount of attempts by 1 
                         with open('database.json', 'w') as f:
                             json.dump(db, f)
 
-                        Fmessage = f'You only have {db["F_attemps"]} attempt(s) left.'
+                        Fmessage = f'You only have {db["F_attemps"]} attempt(s) left.' #here it wil print the amount of attempts you have left
 
                         return render_template('login.html', message=Fmessage)
                 else:
@@ -56,10 +55,6 @@ def login():
 @app.route("/succes")
 def succes():
     return render_template('succes.html')
-
-@app.route("/token")
-def token():
-    return render_template('token.html')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
--- ip blocker/connect.py
+++
@@ -1,8 +0,0 @@
-from email import message
-import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 6060))
-
-message = s.recv(2048)
-
-print(f"Message{message}")
